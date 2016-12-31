ed74877fd061b49a0255f20afccb73abdbc97ff8
ihavenoidea14 <jarid.wade@icloud.com>
2016-12-31 10:25:31 -0500

    Added message in json response

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,13 @@ router.get('/:date', function(req, res, next) {
   if ((/^[0-9]{1,12}/).test(req.params.date)) {
     unix = new Date(parseInt(req.params.date)).getTime();
     natural = new Date(unix).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
-    res.json({ 'unix': unix, 'natural': natural });
+    res.json({ 'message': 'success', 'unix': unix, 'natural': natural });
   } else if (new Date(req.params.date) != 'Invalid Date') {
     natural = new Date(req.params.date).toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
     unix = new Date(natural).getTime();
-    res.json({ 'unix': unix, 'natural': natural });
+    res.json({ 'message': 'success', 'unix': unix, 'natural': natural });
   } else {
-    res.json({ 'unix': null, 'natural': null });
+    res.json({ 'message': 'invalid params', 'unix': null, 'natural': null });
   }
 
 });
