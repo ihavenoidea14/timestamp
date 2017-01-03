70d9ca3c964bd9516d754a6cc32fc23a2404ea71
ihavenoidea14 <jarid.wade@icloud.com>
2017-01-02 19:33:39 -0500

    Added favicon

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
 
 var timestamp = require('./routes/timestamp');
 var index = require('./routes/index');
@@ -9,6 +10,8 @@ var app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
